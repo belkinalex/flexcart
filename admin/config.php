b5d2fe11ca49a3cccb6c84c3baa49962e44edfc9
Alexander Belkin <belkin.alexander@gmail.com>
2019-09-27 15:55:08 +0300

    add configs

---
+++ admin/config.php
@@ -0,0 +1,35 @@
+<?php
+
+// HTTP
+define('HTTP_SERVER', 'http://flexcart.herokuapp.com/admin/');
+define('HTTP_CATALOG', 'http://flexcart.herokuapp.com/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://flexcart.herokuapp.com/admin/');
+define('HTTPS_CATALOG', 'https://flexcart.herokuapp.com/');
+
+// DIR
+//define('DIR_APPLICATION', '/app/admin/');
+//define('DIR_SYSTEM', '/app/system/');
+//define('DIR_LANGUAGE', '/app/admin/language/');
+//define('DIR_TEMPLATE', '/app/admin/view/template/');
+//define('DIR_CONFIG', '/app/system/config/');
+//define('DIR_IMAGE', '/app/image/');
+//define('DIR_CACHE', '/app/system/storage/cache/');
+//define('DIR_DOWNLOAD', '/app/system/storage/download/');
+//define('DIR_UPLOAD', '/app/system/storage/upload/');
+//define('DIR_LOGS', '/app/system/storage/logs/');
+//define('DIR_CATALOG', '/app/catalog/');
+//define('DIR_MODIFICATION', '/app/system/storage/modification/');
+
+// DB
+$db = parse_url($_ENV["CLEARDB_DATABASE_URL"]); 
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', $db["host"]);
+define('DB_USERNAME', $db["user"]);
+define('DB_PASSWORD', $db["pass"]);
+define('DB_DATABASE', trim($db["path"],"/"));
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+?>
