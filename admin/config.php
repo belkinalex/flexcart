983cdf01d6d4a17ff08c361e7b9b7b0c28cd5c40
Alexander Belkin <belkin.alexander@gmail.com>
2019-09-27 16:04:13 +0300

    add configs

--- admin/config.php
+++ admin/config.php
@@ -9,18 +9,19 @@ define('HTTPS_SERVER', 'https://flexcart.herokuapp.com/admin/');
 define('HTTPS_CATALOG', 'https://flexcart.herokuapp.com/');
 
 // DIR
-//define('DIR_APPLICATION', '/app/admin/');
-//define('DIR_SYSTEM', '/app/system/');
-//define('DIR_LANGUAGE', '/app/admin/language/');
-//define('DIR_TEMPLATE', '/app/admin/view/template/');
-//define('DIR_CONFIG', '/app/system/config/');
-//define('DIR_IMAGE', '/app/image/');
-//define('DIR_CACHE', '/app/system/storage/cache/');
-//define('DIR_DOWNLOAD', '/app/system/storage/download/');
-//define('DIR_UPLOAD', '/app/system/storage/upload/');
-//define('DIR_LOGS', '/app/system/storage/logs/');
-//define('DIR_CATALOG', '/app/catalog/');
-//define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_APPLICATION', '/app/admin/');
+define('DIR_SYSTEM', '/app/system/');
+define('DIR_LANGUAGE', '/app/admin/language/');
+define('DIR_TEMPLATE', '/app/admin/view/template/');
+define('DIR_CONFIG', '/app/system/config/');
+define('DIR_IMAGE', '/app/image/');
+define('DIR_CACHE', '/app/system/storage/cache/');
+define('DIR_DOWNLOAD', '/app/system/storage/download/');
+define('DIR_UPLOAD', '/app/system/storage/upload/');
+define('DIR_LOGS', '/app/system/storage/logs/');
+define('DIR_CATALOG', '/app/catalog/');
+define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_STORAGE', '/app/system/storage/');
 
 // DB
 $db = parse_url($_ENV["CLEARDB_DATABASE_URL"]); 
--- config.php
+++ config.php
@@ -8,17 +8,18 @@ define('HTTPS_SERVER', 'https://flexcart.herokuapp.com/');
 
 
 // DIR
-//define('DIR_APPLICATION', '/app/catalog/');
-//define('DIR_SYSTEM', '/app/system/');
-//define('DIR_LANGUAGE', '/app/catalog/language/');
-//define('DIR_TEMPLATE', '/app/catalog/view/theme/');
-//define('DIR_CONFIG', '/app/system/config/');
-//define('DIR_IMAGE', '/app/image/');
-//define('DIR_CACHE', '/app/system/storage/cache/');
-//define('DIR_DOWNLOAD', '/app/system/storage/download/');
-//define('DIR_UPLOAD', '/app/system/storage/upload/');
-//define('DIR_LOGS', '/app/system/storage/logs/');
-//define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_APPLICATION', '/app/catalog/');
+define('DIR_SYSTEM', '/app/system/');
+define('DIR_LANGUAGE', '/app/catalog/language/');
+define('DIR_TEMPLATE', '/app/catalog/view/theme/');
+define('DIR_CONFIG', '/app/system/config/');
+define('DIR_IMAGE', '/app/image/');
+define('DIR_CACHE', '/app/system/storage/cache/');
+define('DIR_DOWNLOAD', '/app/system/storage/download/');
+define('DIR_UPLOAD', '/app/system/storage/upload/');
+define('DIR_LOGS', '/app/system/storage/logs/');
+define('DIR_MODIFICATION', '/app/system/storage/modification/');
+define('DIR_STORAGE', '/app/system/storage/');
 
 // DB
 $db = parse_url($_ENV["CLEARDB_DATABASE_URL"]);
